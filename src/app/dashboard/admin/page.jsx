b6e133b7230d28c17ef2a3c12673ecd5004bfa26
smRid <omorsk911@gmail.com>
2026-06-25 16:44:25 +0600

    create admin dashboard overview page

---
+++ src/app/dashboard/admin/page.jsx
@@ -0,0 +1,9 @@
+import AdminOverview from "@/components/dashboardPage/admin/AdminOverview";
+
+export const metadata = {
+  title: "Admin Dashboard",
+};
+
+export default function AdminDashboardPage() {
+  return <AdminOverview />;
+}
